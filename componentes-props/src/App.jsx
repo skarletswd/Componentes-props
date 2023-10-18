b0f46f7f56ec4c8d31aacfb838790058c282e01f
Andrea Lopez <skarletswdev@gmail.com>
2023-10-18 14:25:22 -0300

    agragamos props a Saludar en App

--- componentes-props/src/App.jsx
+++ componentes-props/src/App.jsx
@@ -1,5 +1,13 @@
+import Saludar from "./Saludar";
+
 function App () {
-    return <h1>Hola</h1>;
+    return (
+    <>
+    <h1>App</h1>
+    <Saludar nombre= "Juan"/>
+    <Saludar nombre= "Maria"/>
+    </>
+    );
 }
 
 export default App;
