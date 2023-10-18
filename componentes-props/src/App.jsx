e85a54ae0ce0fc89d350d8ee953b4f3102b057de
Andrea Lopez <skarletswdev@gmail.com>
2023-10-18 17:21:52 -0300

    comento 3 instancias anteriores

--- componentes-props/src/App.jsx
+++ componentes-props/src/App.jsx
@@ -5,9 +5,9 @@ function App () {
     return (
     <>
         <h1>App</h1>
-        <Saludar nombre= "Juan" datos={{uno: 1, dos: 2}}/>
-        <Saludar nombre= "Maria"/>
-        <Reloj />
+        {/* <Saludar nombre= "Juan" datos={{uno: 1, dos: 2}}/> */}
+        {/* <Saludar nombre= "Maria"/> */}
+        {/* <Reloj /> */}
     </>
     );
 }
